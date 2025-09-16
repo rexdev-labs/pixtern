94e53f074211e80ba2b37cd933fe16dd38659a1c
Ziolicht <ahmadnabilalghifari07@gmail.com>
2025-09-16 20:18:27 +0700

    feat(theme): add brand colors, gradients, and custom fonts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -16,10 +16,50 @@ const config = defineConfig({
                     bg: {
                         black: { value: "#06041C" },
                         white: { value: "#EFFFFF" },
+
+                        // 🔵 Blues
+                        blue: {
+                            primary: { value: "#0781E6" },
+                            sky: { value: "#35A1FB" },
+                            aqua: { value: "#8BE0FF" },
+                            cyan: { value: "#00E6F7" },
+                        },
+
+                        // 🟢 Greens
+                        green: {
+                            emelard: { value: "#12C478" },
+                            mint: { value: "#5CFFA5" },
+                            lime: { value: "#89FF3A" },
+                            softLime: { value: "#D6FF64" },
+                            forest: { value: "#085029" },
+                            lightAqua: { value: "#A7FFF0" },
+                        },
+
+                        // 🟡 Yellows
+                        yellow: {
+                            primary: { value: "#FFBF1C" },
+                            bright: { value: "#FFE424" },
+                        },
+
+                        // 🟣 Purples
+                        purple: { value: "#9D83FE" },
+
+                        // 🩷 Pink
+                        pink: { value: "#F225B3" },
+
+                        // 🟠 Orange
+                        orange: {
+                            value: "#00E6F7",
+                            lightAqua: { value: "#A7FFF0" },
+                        },
                     },
                     text: {
                         black: { value: "#080427" },
                         white: { value: "#EFFFFF" },
+                        gray: { value: "#2A2A2A" },
+                        blue: { value: "#0781E6" },
+                        orange: { value: "#FF6E1D" },
+                        green: { value: "#085029" },
                     },
                     btn: {
                         primary: { value: "#FFBF1C" },
@@ -36,4 +76,4 @@ const config = defineConfig({
     },
 });
 
-export const system = createSystem(defaultConfig, config);
+export const system = createSystem(defaultConfig, config)
