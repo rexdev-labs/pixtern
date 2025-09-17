7bc2c9e321af2f10efe8d647673a2bb8634da272
PrinceTyo <kresnaale2@gmail.com>
2025-09-17 15:26:36 +0700

    merge: integrate font style updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button, HStack } from "@chakra-ui/react";
 import HeroSection from "@/components/sections/HeroSection";
-
+import ProfileSection from "@/components/sections/ProfileSection";
 
 export default function Home() {
-  return (
-    <>
-      <HeroSection />
-    </>
-  );
+    return (
+        <>
+            <HeroSection />
+            <ProfileSection />
+        </>
+    );
 }
